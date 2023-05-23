5ee7a96a37c4366a702a7d9830d6b2fa57cf1f73
timo9939 <timothylau20199@gmail.com>
2023-05-23 19:52:15 +0100

    box css height elongated equally

--- pizza-shop.js
+++ pizza-shop.js
@@ -1,11 +1,25 @@
 let basket=document.getElementById("basket")
 // basket.style.display="none"
+let item
+let quantity
+let price
 
+let add=document.getElementById("add")
 
+add.addEventListener('click', addItem);
+button.addEventListener('click', displayBasket);
 
 function displayBasket(){
 basket.style.display="block"
 console.log("Button Working")
-
+item="Mango Ice-cream"
+document.getElementById("item").innerText=item
 }
+
+
+function addItem(){
+item=200
+document.getElementById("item").innerText=item
+}
+
 
