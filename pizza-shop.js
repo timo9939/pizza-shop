35f4f2e7937d4965f5bca94f05a8617008ad1b3c
timo9939 <timothylau20199@gmail.com>
2023-05-23 22:27:07 +0100

    basket getElementby ID success

--- pizza-shop.js
+++ pizza-shop.js
@@ -1,13 +1,16 @@
 let basket=document.getElementById("basket")
 // basket.style.display="none"
 let item
-let quantity
-let price
+let quantity=0
+let priceProduct
+let i=0
 
-let add=document.getElementById("add")
+let add=document.getElementsByClassName("add")
 
-add.addEventListener('click', addItem);
-button.addEventListener('click', displayBasket);
+for (let i = 0; i < add.length; i++) {
+    add[i].addEventListener('click', addItem);
+
+  }
 
 function displayBasket(){
 basket.style.display="block"
@@ -18,8 +21,16 @@ document.getElementById("item").innerText=item
 
 
 function addItem(){
-item=200
+   
+    console.log("addItem is working")
+    console.log(this.name)
+item=this.name
+priceProduct=document.querySelector(".price").textContent
+console.log("priceProduct is £",priceProduct)
+document.getElementById("price").innerText=priceProduct
 document.getElementById("item").innerText=item
+quantity++
+document.getElementById("quantity").innerText=quantity
 }
 
 
