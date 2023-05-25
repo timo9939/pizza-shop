91a79245e9868592f1bed4e29c98106f1ade8b32
timo9939 <timothylau20199@gmail.com>
2023-05-25 08:32:28 +0100

    index file renamed

--- pizza-shop.js
+++ pizza-shop.js
@@ -1,13 +1,45 @@
-let basket=document.getElementById("basket")
+// let basket=document.getElementById("basket")
 // basket.style.display="none"
 let item
 let quantity=0
 let priceProduct
 let i=0
+let rowCount=2
+let columnCount=2
 
-let add=document.getElementsByClassName("add")
+let add=document.querySelectorAll(".add")
+
+// Create a new table element
+// Get a reference to the table
+const table = document.getElementById('basket');
+
+// Create a new row
+const newRow = table.insertRow();
+
+// Create cells and set content
+const cell1 = newRow.insertCell();
+cell1.textContent = 'Cell 1';
+
+const cell2 = newRow.insertCell();
+cell2.textContent = 'Cell 2';
+
+// Optionally, assign attributes or classes to cells
+cell1.className = 'cellClass';
+cell2.setAttribute('data-info', 'Some data');
+
+// You can continue adding more cells if needed
+
+// Append the new row to the table
+
+// Append the table to an existing element in the DOM
+basket.appendChild(table);
+
+
+
+const productsEl=document.querySelector(".products")
 
 for (let i = 0; i < add.length; i++) {
+  console.log("i.length is",add.length)
     add[i].addEventListener('click', addItem);
 
   }
@@ -19,10 +51,13 @@ item="Mango Ice-cream"
 document.getElementById("item").innerText=item
 }
 
-
+let cart=[];
+ 
 function addItem(){
-    console.log("addItem is working")
-    console.log(this.name)
+  // const item=cart.find((cart) => cart.id===id);
+
+  console.log("addItem is working")
+console.log(this.name)
 item=this.name
 priceProduct=document.querySelector(".price").textContent
 console.log("priceProduct is £",priceProduct)
